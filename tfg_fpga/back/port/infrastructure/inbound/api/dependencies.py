f5c24c8abe6f8832c3712f2f0ecaf4ee1ec59620
Mercedes Villafranca <mercedesvfc@correo.ugr.es>
2026-04-28 09:35:57 +0200

    Eliminar archivos que no se usan

--- tfg_fpga/back/port/infrastructure/inbound/api/dependencies.py
+++ tfg_fpga/back/port/infrastructure/inbound/api/dependencies.py
@@ -8,17 +8,17 @@ from back.port.infrastructure.outbound.mock_port_hardware_adapter import PortHar
 #PONEMOS POR A 4 PARA SIMULAR 4 PUERTOS DE LA ZCU102 SY QUIERA ALVEO200 HABRIA QUE PONER 2
 
 
-hardware = PortHardwareAdapter(port_count=4)
+#hardware = PortHardwareAdapter(port_count=4)
 repo = ElasticsearchRepository()
-service = PortCountersService(hardware, repo)
 
 mode = os.getenv("mode", "simulation").lower()
 
 if mode == "real":
      # Fase 2: aqui iria el adaptador para la fpga real
-    null
+    raise NotImplementedError("Adaptador hardware real pendiente — Fase 2")
 else:
     # Fase 1: Simulación local 
     hardware = PortHardwareAdapter(port_count=4)
+    service = PortCountersService(hardware, repo)
 def get_port_service():
     return service
