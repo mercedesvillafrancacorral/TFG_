ebdd3c9e0f49c55662125de823db2400eb3081de
Mercedes Villafranca <mercedesvillafrancacorral@gmail.com>
2026-08-22 12:53:27 +0200

    corregir error

--- tfg_fpga/back/port/application/IPortHardware.py
+++ tfg_fpga/back/port/application/IPortHardware.py
@@ -1,6 +1,7 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 from back.port.domain.PortCounters import PortCounters
-from __future__ import annotations
+
 
 
 class IPortHardware(ABC):
