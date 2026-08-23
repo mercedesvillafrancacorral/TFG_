40028d9d2cf32bd0e2908ecca6b6b63a0b771c0c
Mercedes Villafranca <mercedesvillafrancacorral@gmail.com>
2026-08-23 16:55:34 +0200

    Evitar que rompa la app cuando un puerto devuelve datos corruptos

--- tfg_fpga/back/port/application/IPortHardware.py
+++ tfg_fpga/back/port/application/IPortHardware.py
@@ -7,7 +7,7 @@ from back.port.domain.PortCounters import PortCounters
 class IPortHardware(ABC):
     @abstractmethod
     def read_counters(self, port_id: int) -> PortCounters:
-        pass
+        pass 
 
     @abstractmethod
     def get_ports(self)->list[int]:
