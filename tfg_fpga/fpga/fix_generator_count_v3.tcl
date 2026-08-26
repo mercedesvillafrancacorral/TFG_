0e0ad0c38863efe74b5d75aeea211ec15d7fb5b7
Mercedes Villafranca <mercedesvillafrancacorral@gmail.com>
2026-08-26 10:51:35 +0200

    arregalr el tcl fix generator

--- tfg_fpga/fpga/fix_generator_count_v3.tcl
+++ tfg_fpga/fpga/fix_generator_count_v3.tcl
@@ -1,20 +1,45 @@
 set project_path "/home/mvillafranca/traffic_generator_mercedes/fpga/tool/ZCU102/fpga_traffic_generator/fpga/fpga.xpr"
 open_project $project_path
 
-set current_generics [get_property generic [get_filesets sources_1]]
-puts "Generic antes (completo): $current_generics"
+puts "Generic antes (danado por un intento anterior): [get_property generic [get_filesets sources_1]]"
 
-set new_generics {}
-foreach g $current_generics {
-    if {[string match "PORT_GEN_TRAFFIC_COMMON_COUNT=*" $g]} {
-        lappend new_generics "PORT_GEN_TRAFFIC_COMMON_COUNT=10"
-    } else {
-        lappend new_generics $g
-    }
-}
-set_property generic $new_generics [get_filesets sources_1]
+# Restaura la lista COMPLETA original de generics (capturada antes de que un
+# intento anterior la reemplazara por error con un solo valor), con
+# PORT_GEN_TRAFFIC_COMMON_COUNT cambiado de 3 a 10.
+set full_generics [list \
+    "AXIS_ETH_100G_DATA_WIDTH=512" \
+    "AXIS_ETH_100G_KEEP_WIDTH=64" \
+    "AXIS_ETH_25G_DATA_WIDTH=64" \
+    "AXIS_ETH_25G_KEEP_WIDTH=8" \
+    {PHY_ENABLE=4'b1111} \
+    {PORT_ENABLE=4'b1111} \
+    "PORT_READ_WIDTH=64" \
+    "PORT_RX_TRAFFIC_ENABLE=1" \
+    "PORT_TX_TRAFFIC_ENABLE=1" \
+    "PORT_GEN_TRAFFIC_ENABLE=1" \
+    "PORT_GEN_TRAFFIC_COMMON_COUNT=10" \
+    "PORT_GEN_TRAFFIC_RAM_COUNT=2" \
+    "PORT_GEN_TRAFFIC_RAM_TARGET_COUNT=512" \
+    "PORT_GEN_COUNTER_WIDTH=16" \
+    "PORT_GEN_COUNTER_FRAC_WIDTH=16" \
+    "PORT_GEN_MIN_FRAME_LENGTH=60" \
+    "PORT_GEN_OUTPUT_REGISTER=1" \
+    "CH_COUNT=4" \
+    {VC_COUNT=32'h01010101} \
+    {VC_RATE=32'h00000000} \
+    "INGRESS_25G_FIFO_DEPTH=2048" \
+    "EGRESS_25G_FIFO_DEPTH=16384" \
+    "INGRESS_100G_FIFO_DEPTH=2048" \
+    "EGRESS_100G_FIFO_DEPTH=16384" \
+    "INTERMEDIATE_FREQUENCY=250000000" \
+    "BAUD_RATE=115200" \
+    "TEST_AXIL_PIPE_COUNT=0" \
+    "PORT_AXIL_PIPE_COUNT=0" \
+]
 
-puts "Generic despues (completo): [get_property generic [get_filesets sources_1]]"
+set_property generic $full_generics [get_filesets sources_1]
+
+puts "Generic despues (restaurado): [get_property generic [get_filesets sources_1]]"
 
 reset_run synth_1
 launch_runs synth_1 -jobs 8
